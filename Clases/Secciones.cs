2724c2ed87325ee220ce8a9b2e94a68b3a71e4b9
CESAR LEPE GARCIA <L22212360@tijuana.tecnm.mx>
2023-05-30 22:35:52 -0700

    TerminadaResumenPag

---
+++ Clases/Secciones.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+
+namespace HIITT.Clases
+{
+    class Secciones
+    {
+        public static void GenerarTitulos(string texto, StackPanel mainStackPanel)
+        {
+            TextBlock txb = new();
+            txb.Text = texto;
+            txb.HorizontalAlignment = HorizontalAlignment.Left;
+            txb.Margin = new Thickness(10, 10, 0, 0);
+            mainStackPanel.Children.Add(txb);
+        }
+        public static void GenerarSubTitulos(string texto, StackPanel mainStackPanel)
+        {
+            TextBlock txb = new();
+            txb.Text = texto;
+            txb.HorizontalAlignment = HorizontalAlignment.Left;
+            txb.Margin = new Thickness(10, 10, 0, 0);
+            mainStackPanel.Children.Add(txb);
+        }
+        public static void GenerarTextoNormal(string texto, StackPanel mainStackPanel)
+        {
+            TextBlock txb = new();
+            txb.Text = texto;
+            txb.HorizontalAlignment = HorizontalAlignment.Left;
+            txb.Margin = new Thickness(10, 10, 0, 0);
+            mainStackPanel.Children.Add(txb);
+        }
+    }
+}
--- Paginas/ResumenPag.xaml.cs
+++ Paginas/ResumenPag.xaml.cs
@@ -28,28 +28,20 @@ namespace GimApp
             _mainFrame = mainPage;
             GenerarDia("Hoy");
             GenerarDia("Manaña");
+            GenerarHorarioDiario();
         }
         Frame _mainFrame;
 
-        public void GenerarDia(string dia) 
+        public void GenerarDia(string dia)
         {
-            GenerarTitulos(dia);
+            Secciones.GenerarTitulos(dia, MainStackPanel);
             string[] rutinasActivasPath = ManejadorTextos.RutinasActivasPathList();
             if (rutinasActivasPath.Length > 0)
                 GenerarRutinas(rutinasActivasPath, dia);
             else
                 GenerarNoRutinasHoy();
         }
-        //Textos genericos
-        public void GenerarTitulos(string texto)
-        {
-            TextBlock txb = new();
-            txb.Text = texto;
-            txb.HorizontalAlignment = HorizontalAlignment.Left;
-            txb.Margin = new Thickness(10, 10, 0, 0);
-            MainStackPanel.Children.Add(txb);
-        }
-        //Rutinas de hoy
+
         public void GenerarRutinas(string[] rutinasActivasPath, string dia)
         {
             if (dia == "Hoy")
@@ -97,7 +89,25 @@ namespace GimApp
 
             MainStackPanel.Children.Add(grd);
         }
-
+        public void GenerarHorarioDiario()
+        {
+            Secciones.GenerarTitulos("Horario Diario",MainStackPanel);
+            string[] rutinasActivas = ManejadorTextos.RutinasActivasPathList();
+            string[] diasDeLaSemana = { "Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado" };
+            string[] diasDeLaSemanaIngles = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            for (int i = 0; i < 7; i++) {
+                Secciones.GenerarSubTitulos(diasDeLaSemana[i],MainStackPanel);
+                bool bandera = false;
+                foreach (string rutinaPath in rutinasActivas)
+                    if (ManejadorTextos.LeerDiaRutina(rutinaPath) == diasDeLaSemanaIngles[i])
+                    {
+                        Secciones.GenerarTextoNormal(ManejadorTextos.LeerNombreRutina(rutinaPath), MainStackPanel);
+                        bandera = true;
+                    }
+                if (!bandera)
+                    Secciones.GenerarTextoNormal($"No hay rutinas asignadas al dia {diasDeLaSemana[i].ToLower()}", MainStackPanel);
+            }
+        }
         private void AgregarRutina(object sender, RoutedEventArgs e)
         {
             _mainFrame.Content = new AgregarRutinaPag(_mainFrame);
